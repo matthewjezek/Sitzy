7f1c6198fbf6bfa855de138cbd0cf5ffb9f0a45f
Matěj Ježek <jetam.jezek@gmail.com>
2025-06-24 23:59:26 +0200

    Add user schemas for creation, login, and output representation

---
+++ api/schemas.py
@@ -0,0 +1,24 @@
+from typing import Annotated
+from pydantic import BaseModel, EmailStr, StringConstraints
+from uuid import UUID
+from datetime import datetime
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+
+class UserCreate(UserBase):
+    password: Annotated[str, StringConstraints(min_length=8)]
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserOut(UserBase):
+    id: UUID
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
