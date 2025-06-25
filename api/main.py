bb629a28a891c9ba27c941b298ddb786857d8058
Matěj Ježek <jetam.jezek@gmail.com>
2025-06-25 19:19:52 +0200

    Add invitations router to main application

--- api/main.py
+++ api/main.py
@@ -27,9 +27,9 @@ app.add_middleware(
 app.add_middleware(LanguageMiddleware)
 
 # Import routerů
-from api.routers import auth, cars
+from api.routers import auth, cars, invitations
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(cars.router, prefix="/cars", tags=["cars"])
 # app.include_router(seat.router, prefix="/seats", tags=["seats"])
-# app.include_router(invitation.router, prefix="/invitations", tags=["invitations"])
+app.include_router(invitations.router, prefix="/invitations", tags=["invitations"])
