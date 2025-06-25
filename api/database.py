d145d3fa7702bbc88677ed19cf0524efd6e75147
Matěj Ježek <jetam.jezek@gmail.com>
2025-06-25 21:13:50 +0200

    Refactor database.py to improve import statements and update Base class definition

--- api/database.py
+++ api/database.py
@@ -1,8 +1,9 @@
 import os
+from collections.abc import Generator
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
 
 # Načtení proměnných z .env
 load_dotenv()
@@ -18,12 +19,14 @@ engine = create_engine(DATABASE_URL, echo=True, future=True)
 # Session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
 # Deklarativní základna pro modely
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 
 # Funkce pro získání databázové relace
-def get_db():
+def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db
