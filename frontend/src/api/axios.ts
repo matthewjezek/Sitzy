47d39a648f7580c0d988506d3e0de0c1502aed3b
Matěj Ježek <jetam.jezek@gmail.com>
2025-08-17 15:49:46 +0200

    Refactor axios instance creation

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,35 +1,19 @@
 import axios from 'axios'
-import { useNavigate } from "react-router"
 
-const AxiosInstance = () => {
-  const navigate = useNavigate()
-  const token = localStorage.getItem('token')
-  const instance = axios.create({
-    baseURL: 'http://localhost:8000',
-    timeout: 10000, // 10 seconds timeout
-  });
+const instance = axios.create({
+  baseURL: 'http://localhost:8000',
+  timeout: 10000, // 10 seconds timeout
+})
 
-  instance.interceptors.request.use(
-    config => {
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`
-      }
-      return config
-    },
-    error => Promise.reject(error)
-  )
-  
-
-  instance.interceptors.response.use(
-    response => response,
-    error => {
-      if (error.response && error.response.status === 401) {
-        localStorage.removeItem('token')
-        navigate('/login?expired=1')
-      }
-      return Promise.reject(error)
+instance.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
     }
-  )
-  return instance
-}
-export default AxiosInstance
+    return config
+  },
+  error => Promise.reject(error)
+)
+
+export default instance
