eb523c6461371d9a3880ed53f497ad7d61be75ca
Matěj Ježek <jetam.jezek@gmail.com>
2025-08-18 05:07:17 +0200

    Refactor App component to comment out unused SeatPage import and add new routes for SeatRendererTestPage, SeatRendererDemo, SeatPositionTest, and update SeatPage route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import DashboardPage from './pages/DashboardPage'
-import SeatPage from './pages/SeatPage'
+// import SeatPage from './pages/SeatPage'
 import InvitationListPage from './pages/InvitePage'
 import CarPage from './pages/CarPage'
 import CreateCarPage from './pages/CreateCarPage'
@@ -10,8 +10,11 @@ import Layout from './components/Layout'
 import ProtectedRoute from './components/ProtectedRoute'
 import InvitePage from "./pages/CreateInvitePage";
 import PageNotFound from './pages/PageNotFound'
+import SeatRendererTestPage from './pages/SeatRendererTestPage';
+import SeatRendererDemo from './pages/SeatRendererDemo';
+import SeatPositionTest from './pages/SeatPositionTest';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import SeatPageNew from './pages/SeatPageNew'
 
 function App() {
   return (
@@ -28,11 +31,15 @@ function App() {
         {/* Stránky pod layoutem */}
         <Route element={<ProtectedRoute><Layout /></ProtectedRoute>}>
           <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/seats" element={<SeatPage />} />
+          {/* <Route path="/seats" element={<SeatPage />} /> */}
           <Route path="/invitations" element={<InvitationListPage />} />
           <Route path="/car" element={<CarPage />} />
           <Route path="/create-car" element={<CreateCarPage />} />
           <Route path="/invite" element={<InvitePage />} />
+          <Route path="/test-seats" element={<SeatRendererTestPage />} />
+          <Route path="/demo-seats" element={<SeatRendererDemo />} />
+          <Route path="/position-test" element={<SeatPositionTest />} />
+          <Route path="/seats" element={<SeatPageNew />} />
         </Route>
       </Routes>
       <ToastContainer position="bottom-left" autoClose={3000} hideProgressBar pauseOnHover draggable theme="colored" />
