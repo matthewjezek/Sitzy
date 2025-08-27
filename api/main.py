13c292a8e87399898a7af92936e77625cf430522
Matěj Ježek <jetam.jezek@gmail.com>
2025-08-27 23:34:00 +0200

    Odebrány zbytečné komentáře

--- api/main.py
+++ api/main.py
@@ -29,13 +29,12 @@ app.add_middleware(
 async def validation_exception_handler(
     request: Request, exc: RequestValidationError
 ) -> JSONResponse:
-    # Můžeš si výstup upravit dle libosti, třeba jen první chybu nebo vše jako string
     error_messages = [f"{err['loc'][-1]}: {err['msg']}" for err in exc.errors()]
     return JSONResponse(
         status_code=422,
         content={
             "detail": "; ".join(error_messages)
-        },  # nebo {"detail": error_messages} pro pole
+        },
     )
 
 
