459090c40c3b387e94fb98ccce5817d4f6764f8b
Matěj Ježek <jetam.jezek@gmail.com>
2025-09-01 01:37:13 +0200

    Upravení stylu a struktury komponenty PageNotFound pro konzistentní design a zlepšení uživatelského rozhraní

--- frontend/src/pages/PageNotFound.tsx
+++ frontend/src/pages/PageNotFound.tsx
@@ -5,9 +5,9 @@ const PageNotFound = () => {
 
   return (
     <html>
-      <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
-        <div className="text-center">
-          <p className="text-base font-semibold text-indigo-600">404</p>
+      <main className="page-container text-center">
+        <div className="page-content">
+          <p className="text-base font-semibold text-indigo-500">404</p>
           <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
             Stránka nenalezena
           </h1>
@@ -15,10 +15,10 @@ const PageNotFound = () => {
             Omlouváme se, ale stránku, kterou hledáte, jsme nenašli.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <button onClick={() => navigate(-1)} className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer">
+            <button onClick={() => navigate(-1)} className="primary-button">
               Jít zpět
             </button>
-            <a href="#" className="text-sm font-semibold text-gray-900">
+            <a href="#" className="text-sm font-semibold text-gray-700">
               Contact support <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
