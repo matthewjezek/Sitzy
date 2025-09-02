fb0da1495d72d2a4fe8627f4090ff0fbae379073
Matěj Ježek <jetam.jezek@gmail.com>
2025-09-02 21:40:02 +0200

    Odstranění staré stránky pro pozvánky a stránky pro výběr míst, úprava routování v App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import DashboardPage from './pages/DashboardPage'
-// import SeatPage from './pages/SeatPage'
-import InvitationListPage from './pages/InvitePage'
 import CarPage from './pages/CarPage'
 import { CreateCarPage } from './pages/CreateCarPage'
 import Layout from './components/Layout'
@@ -34,8 +32,6 @@ function App() {
         {/* Stránky pod layoutem */}
         <Route element={<ProtectedRoute><Layout /></ProtectedRoute>}>
           <Route path="/dashboard" element={<DashboardPage />} />
-          {/* <Route path="/seats" element={<SeatPage />} /> */}
-          <Route path="/invitations" element={<InvitationListPage />} />
           <Route path="/car" element={<CarPage />} />
           <Route path="/create-car" element={<CreateCarPage />} />
           <Route path="/edit-car" element={<CreateCarPage editMode={true} />} />
