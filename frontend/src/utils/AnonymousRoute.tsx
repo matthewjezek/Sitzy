e27c02799fe93c0eb9686f5da1bb32c880851ef4
Matěj Ježek <jetam.jezek@gmail.com>
2026-03-01 21:55:26 +0100

    fix: Update token retrieval in AnonymousRoute and ProtectedRoute to use 'access_token'

--- frontend/src/utils/AnonymousRoute.tsx
+++ frontend/src/utils/AnonymousRoute.tsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from "react-router";
 
 export default function AnonymousRoute() {
-  const token = localStorage.getItem('token')
+  const token = localStorage.getItem('access_token')
 
   return token ? <Navigate to="/" replace /> : <Outlet />;
 }
--- frontend/src/utils/ProtectedRoute.tsx
+++ frontend/src/utils/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import type { JSX } from 'react'
 import { Navigate } from 'react-router'
 
 export default function ProtectedRoute({ children }: { children: JSX.Element }) {
-  const token = localStorage.getItem('token')
+  const token = localStorage.getItem('access_token')
   if (!token) {
     return <Navigate to="/login" replace />
   }
