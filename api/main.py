a99ac7212ebd63828fee259be41cc686756892d6
Matěj Ježek <jetam.jezek@gmail.com>
2026-03-02 20:33:59 +0100

    fix: add OPTIONS method to CORS middleware configuration

--- api/main.py
+++ api/main.py
@@ -38,7 +38,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[settings.frontend_origin],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PATCH", "DELETE"],
+    allow_methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["Authorization", "Content-Type"],
 )
 
