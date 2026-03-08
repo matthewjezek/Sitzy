42b0a2e432b26c72d3b7f674be0a9d25ac714b5b
Matěj Ježek <jetam.jezek@gmail.com>
2026-03-08 15:46:48 +0100

    feat: add accessibility checks and update ESLint configuration

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
@@ -18,6 +19,29 @@ export default tseslint.config([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      'jsx-a11y': jsxA11y,
+    },
+    rules: {
+      // A11y pravidla pro akademickou auditovatelnost
+      'jsx-a11y/alt-text': 'warn',
+      'jsx-a11y/aria-props': 'error',
+      'jsx-a11y/aria-proptypes': 'error',
+      'jsx-a11y/aria-unsupported-elements': 'error',
+      'jsx-a11y/role-has-required-aria-props': 'error',
+      'jsx-a11y/role-supports-aria-props': 'error',
+      'jsx-a11y/label-has-associated-control': ['warn', {
+        'required': {
+          'some': ['nesting', 'id']
+        }
+      }],
+      'jsx-a11y/click-events-have-key-events': 'warn',
+      'jsx-a11y/no-static-element-interactions': 'warn',
+      'jsx-a11y/interactive-supports-focus': 'warn',
+      'jsx-a11y/img-redundant-alt': 'warn',
+      'jsx-a11y/heading-has-content': 'warn',
+      'jsx-a11y/anchor-is-valid': 'warn',
     },
   },
 ])
