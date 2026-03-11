658d8c901f1e6d2474a461f8328c4cfef66354a9
Matěj Ježek <jetam.jezek@gmail.com>
2026-03-11 23:43:15 +0100

    Revert "test: change VitePWA registerType from 'autoUpdate' to 'prompt'"
    This reverts commit c334ad4997ebd74edc6a14ec430c1edd663e0550.

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
     VitePWA({
-      registerType: 'prompt',
+      registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'favicon.svg', 'apple-touch-icon.png', 'icons/*.png', 'splash/*.png'],
       manifest: {
         name: 'Sitzy',
